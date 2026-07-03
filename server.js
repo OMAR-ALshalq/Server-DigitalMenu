60b2907e7c478736c08c67d6e62528e70d108f3e
OMAR-ALshalq <omaralshalq@gmail.com>
2026-07-03 16:30:01 +0300

    edit server.js

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const io = new Server(server, {
       "http://localhost:5173",
       "http://localhost:5174",
       ,
-      "http://localhost:3000"
+      "http://localhost:3000",
+      "https://server-digitalmenu.onrender.com"
     ],
     methods: ["GET", "POST"]
   }
